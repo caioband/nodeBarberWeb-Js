7049be79143d80607ce27a3aaa900f03413429c1
caioband <caioobsantos@gmail.com>
2024-11-08 15:27:24 -0300

    a
    a

---
+++ exceções.js
@@ -0,0 +1,19 @@
+import {Prisma} from '@prisma/client'
+
+export async function Exception(f){
+
+    try {
+        let Data = await f()
+        
+        return {data: Data, statusCode: 200}
+    } catch(e){
+        
+        if (e.code === 'P2023'){
+            return {messageError: "Column not found", statusCode: 404}
+            
+        }
+        if (e.code === 'P2025'){
+            return {messageError: "Trying to delete a null value", statusCode: 404}
+        }
+    }
+}
